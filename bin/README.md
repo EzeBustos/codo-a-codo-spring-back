da2d8d275dee2e4be8c44a5ad871dbc1ea5e0216
Ezequiel <ezalbu13@gmail.com>
2022-09-13 19:06:58 -0300

    clases/metodos/objetos

---
+++ bin/README.md
@@ -0,0 +1,4 @@
+# codo-a-codo-spring-back
+codo-a-codo-spring-back
+
+clase1: creamos repo back y front
---
+++ src/main/java/ar/com/codoacodo/Main.java
@@ -0,0 +1,17 @@
+package ar.com.codoacodo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Auto car1 = new Auto();
+		
+		car1.mostrarInfo();
+		
+		car1.encender();
+		
+		car1.mostrarInfo();
+		
+	}
+
+}
